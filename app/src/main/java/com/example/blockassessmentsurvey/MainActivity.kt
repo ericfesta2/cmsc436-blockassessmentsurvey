11507b2b81f8365aea2f031e6b8b6918026aa596
Benjamin Beinish <benjaminbeinish@gmail.com>
2020-11-09 19:29:16 -0500

    created layouts for home page and review page. Put in code to navigate between the two.

--- app/src/main/java/com/example/blockassessmentsurvey/MainActivity.kt
+++ app/src/main/java/com/example/blockassessmentsurvey/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.blockassessmentsurvey
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SearchView
+import android.widget.SeekBar
+import android.widget.TextView
+import com.google.android.gms.maps.MapView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mMap = findViewById<MapView>(R.id.mapView)
+        val mRadius = findViewById<SeekBar>(R.id.radiusSeek)
+        val mSearch = findViewById<SearchView>(R.id.blockSearch)
+        val milesTextView = findViewById<TextView>(R.id.radiusTextView)
+        val mButton = findViewById<Button>(R.id.reviewLocationButton)
+
+        mButton.setOnClickListener() {
+            val intent = Intent(this, ReviewActivity::class.java)
+            //TODO:: Include currently selected block as an extra
+            startActivity(intent)
+        }
+
     }
 }
