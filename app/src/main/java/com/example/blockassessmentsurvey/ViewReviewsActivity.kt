41c9df97e89a201672d335508846ae61609d9ccd
Eric Festa <eric.festa2@gmail.com>
2020-11-11 14:57:19 -0500

    Added floating action button for new reviews and labels to show the number of ratings for each category

--- app/src/main/java/com/example/blockassessmentsurvey/ViewReviewsActivity.kt
+++ app/src/main/java/com/example/blockassessmentsurvey/ViewReviewsActivity.kt
@@ -1,12 +1,21 @@
 package com.example.blockassessmentsurvey
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ViewReviewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page_review)
 
+        findViewById<FloatingActionButton>(R.id.addReviewButton).setOnClickListener() {
+            val newReviewIntent = Intent(this, ReviewActivity::class.java)
+
+            newReviewIntent.resolveActivity(packageManager)?.let {
+                startActivity(newReviewIntent)
+            }
+        }
     }
 }
