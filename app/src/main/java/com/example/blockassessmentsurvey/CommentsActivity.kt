14c826ddeb18320b25930ac04a6971c38db597d0
Eric Festa <eric.festa2@gmail.com>
2020-11-12 18:05:20 -0500

    Separate comments screen, new button

---
+++ app/src/main/java/com/example/blockassessmentsurvey/CommentsActivity.kt
@@ -0,0 +1,23 @@
+package com.example.blockassessmentsurvey
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+class CommentsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.comments_page)
+        setTitle("Block Name") // TODO: getExtra
+
+        val reviewList = listOf(
+                Review("Eric", "11/11/2020", "Testing!"),
+                Review("Test User", "5/12/2013", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                Review("Test User", "5/12/2013", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+        )
+
+        val reviewAdapter = ReviewList(this, reviewList)
+        findViewById<ListView>(R.id.reviewsList).adapter = reviewAdapter
+    }
+}
--- app/src/main/java/com/example/blockassessmentsurvey/ViewReviewsActivity.kt
+++ app/src/main/java/com/example/blockassessmentsurvey/ViewReviewsActivity.kt
@@ -2,6 +2,7 @@ package com.example.blockassessmentsurvey
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -10,6 +11,7 @@ class ViewReviewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page_review)
+        setTitle("Block Name") // TODO: getExtra
 
         findViewById<FloatingActionButton>(R.id.addReviewButton).setOnClickListener() {
             val newReviewIntent = Intent(this, ReviewActivity::class.java)
@@ -19,13 +21,22 @@ class ViewReviewsActivity : AppCompatActivity() {
             }
         }
 
+        findViewById<FloatingActionButton>(R.id.viewCommentsBtn).setOnClickListener() {
+            val commentsIntent = Intent(this, CommentsActivity::class.java)
+            // TODO: putExtra block name
+
+            commentsIntent.resolveActivity(packageManager)?.let {
+                startActivity(commentsIntent)
+            }
+        }
+
         // TODO: Firebase integration
-        val reviewList = listOf(
+        /*val reviewList = listOf(
             Review("Eric", "11/11/2020", "Testing!"),
             Review("Test User", "5/12/2013", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
         )
 
         val reviewAdapter = ReviewList(this, reviewList)
-        findViewById<ListView>(R.id.reviewsList).adapter = reviewAdapter
+        findViewById<ListView>(R.id.reviewsList).adapter = reviewAdapter*/
     }
 }
