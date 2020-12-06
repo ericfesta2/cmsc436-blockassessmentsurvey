5e196fd7b728dd88f3df502e37775aa8c19d744c
Benjamin Beinish <benjaminbeinish@gmail.com>
2020-12-06 18:50:50 -0500

    about to merge Eric's branch

--- app/src/main/java/com/example/blockassessmentsurvey/CommentsActivity.kt
+++ app/src/main/java/com/example/blockassessmentsurvey/CommentsActivity.kt
@@ -2,21 +2,61 @@ package com.example.blockassessmentsurvey
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.lang.Exception
 
 class CommentsActivity : AppCompatActivity() {
+
+    val reviewList = mutableListOf<Review>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.comments_page)
-        setTitle("Block Name") // TODO: getExtra
 
-        val reviewList = listOf(
-                Review("Eric", 5f, 5f, 5f, 5f, "11/11/2020", "Testing!"),
-                Review("Test User", 5f, 5f, 5f, 5f,"5/12/2013", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
-                Review("Test User", 5f, 5f, 5f, 5f,"5/12/2013", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-        )
+        val state = intent.getStringExtra("State")
+        val city = intent.getStringExtra("City")
+        val street = intent.getStringExtra("Street")
 
+        val path = state + "/" + city + "/" + street
+
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference(path)
+
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                Log.i("View Reviews", dataSnapshot.toString())
+                val reviews = dataSnapshot.value as Map<*, Map<String, *>>
+                val reviews1 = reviews.values
+                for (review in reviews1) {
+                    try {
+                        Log.i("View Comments", review.get("comments").toString())
+                        val r = Review(review["reviewer"] as String, 0f, 0f, 0f, 0f, review["posted"] as String, review["comments"] as String)
+                        reviewList.add(r)
+                        update()
+                    } catch (e: Exception) {
+                        Log.i("View Comments", e.toString())
+
+                    } finally {
+
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        })
+
+    }
+
+    fun update() {
         val reviewAdapter = ReviewList(this, reviewList)
         findViewById<ListView>(R.id.reviewsList).adapter = reviewAdapter
     }
